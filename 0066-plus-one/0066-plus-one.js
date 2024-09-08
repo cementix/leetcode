548b54f74aa902e51582f27f60b0d816ee7d0adb
Bohdan Kondratskyi <144433478+cementix@users.noreply.github.com>
2024-09-08 23:19:02 +0300

    Time: 66 ms (6.60%), Space: 48.9 MB (43.37%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -3,22 +3,14 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    digits[digits.length - 1] += 1
-
-        for (let i = digits.length - 1; i >= 0; i--) {
-                if (digits[i] === 10) {
-                    digits[i] = 0
-                    if (!!digits[i - 1]) {
-                        digits[i - 1] += 1
-                    } else {
-                        digits.unshift(1)
-                    }
-                } else {
-                    break
-                }
+    let n = digits.length;
+    for (let i = n - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
         }
-    
-    return digits
-    
-
+        digits[i] = 0;
+    }
+    digits.unshift(1); // Only happens when all digits are 9
+    return digits;
 };
