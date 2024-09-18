cebed4f7d8d3e3e8b94c2bf4c5c061fc6fbe5446
Bohdan Kondratskyi <144433478+cementix@users.noreply.github.com>
2024-09-18 13:46:59 +0300

    Time: 70 ms (32.97%), Space: 54.1 MB (20.34%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -1,9 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var singleNumber = function(nums) {
-    let result = 0;
+    const uniqueNums = new Set(nums);  // Множество уникальных чисел
+    let sumUnique = 0;  // Сумма уникальных чисел
+    let sumAll = 0;  // Сумма всех чисел
 
-    for (let num of nums) {
-        result ^= num;
+    // Считаем сумму всех уникальных чисел
+    for (let num of uniqueNums) {
+        sumUnique += num;
     }
 
-    return result;
+    // Считаем сумму всех чисел
+    for (let num of nums) {
+        sumAll += num;
+    }
+
+    // Используем формулу: 2 * (сумма уникальных) - (сумма всех)
+    return 2 * sumUnique - sumAll;
 };
