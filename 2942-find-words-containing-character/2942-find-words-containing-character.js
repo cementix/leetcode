55150cab5d17aca0188e135b57301de171d18060
Bohdan Kondratskyi <144433478+cementix@users.noreply.github.com>
2024-09-18 13:50:37 +0300

    Time: 74 ms (61.39%), Space: 53.3 MB (28.19%) - LeetHub

--- 2942-find-words-containing-character/2942-find-words-containing-character.js
+++ 2942-find-words-containing-character/2942-find-words-containing-character.js
@@ -4,13 +4,13 @@
  * @return {number[]}
  */
 var findWordsContaining = function(words, x) {
-  const indices = []
+  const indices = [];
   
-  for (let i = 0; i < words.length; i++) {
-      if (words[i].split('').includes(x)) {
-          indices.push(i)
+  words.forEach((word, index) => {
+      if (word.indexOf(x) !== -1) {
+          indices.push(index);
       }
-  }
-    
-    return indices
+  });
+
+  return indices;
 };
