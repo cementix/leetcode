4d051cba5d6fe1b5ea59c30645ede55a6c0c3613
Bohdan Kondratskyi <144433478+cementix@users.noreply.github.com>
2024-09-27 12:08:28 +0300

    Time: 47 ms (86.05%), Space: 48.8 MB (73.75%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -1,6 +1,21 @@
 var merge = function(nums1, m, nums2, n) {
-    for (let i = m, j = 0; i < m + n; i++, j++) {
-        nums1[i] = nums2[j]
+    if (m === 0 && n === 1) {
+        nums1[m] = nums2[n - 1]
+        return
     }
-    nums1.sort((a,b) => a - b)
+    
+   let second = nums1.length - 1
+   let first = m - 1
+   n--
+    
+    while(n >= 0) {
+        if (first >= 0 && nums1[first] > nums2[n]) {
+            nums1[second] = nums1[first]
+            first--
+        } else {
+            nums1[second] = nums2[n]
+            n--
+        }
+        second--
+    }
 };
