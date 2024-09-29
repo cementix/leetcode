900a01997aa515a6e1028e45d2232beceee2ee87
Bohdan Kondratskyi <144433478+cementix@users.noreply.github.com>
2024-09-29 14:23:48 +0200

    Time: 3 ms (90.16%), Space: 4.3 MB (13.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,10 +1,13 @@
 func twoSum(nums []int, target int) []int {
-    for i := 0; i < len(nums); i++ {
-        for j := i + 1; j < len(nums); j++ {
-            if nums[i] + nums[j] == target {
-                return []int{i, j}
-            }
+    numMap := make(map[int]int)
+    
+    for i, num := range nums {
+        complement := target - num
+        if index, ok := numMap[complement]; ok {
+            return []int{index, i}
         }
+        numMap[num] = i
     }
-    return nil
+    
+    return nil // return nil if no solution
 }
