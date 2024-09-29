bdb1af783acb980c0a80ce09992b4c4c2bc8f80a
Bohdan Kondratskyi <144433478+cementix@users.noreply.github.com>
2024-09-29 09:30:18 +0200

    Time: 55 ms (81.69%), Space: 51.8 MB (57.93%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -1,13 +1,14 @@
 var majorityElement = function (nums) {
-    let count = 0;
-    let candidate = 0;
-
-    for (let num of nums) {
+    let count = 0
+    let candidate = 0
+    
+    for (const num of nums) {
         if (count === 0) {
-            candidate = num;
+            candidate = num
         }
-        count += (num === candidate) ? 1 : -1;
+
+        count += (candidate === num) ? 1 : -1
     }
 
-    return candidate;
+    return candidate
 };
