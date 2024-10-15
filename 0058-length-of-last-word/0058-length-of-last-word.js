7a09363aa507ba8eaeeaa2f316db8d3e51602546
Bohdan Kondratskyi <144433478+cementix@users.noreply.github.com>
2024-10-15 16:26:06 +0200

    Time: 50 ms (65.69%), Space: 49.2 MB (12.22%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,17 +3,5 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    let length = 0
-    let found = false
-
-    for (let i = s.length - 1; i >= 0; i--) {
-            if (s[i] !== " ") {
-                length++
-                found = true
-            } else if (s[i] === " " && found === true) {
-                break
-            }
-    }
-
-    return length
+    return s.trim().split(" ").pop().length;
 };
