4acd5a596490ec70cb0a02766c90bf1771dec8bb
Bohdan Kondratskyi <144433478+cementix@users.noreply.github.com>
2024-10-16 10:10:25 +0200

    Time: 53 ms (68.16%), Space: 49.2 MB (66.86%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,13 @@
+function longestCommonPrefix(strs) {
+    if (!strs.length) return "";
+
+    let prefix = strs[0];
+
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, prefix.length - 1);
+            if (!prefix) return "";
+        }
+    }
+    return prefix;
+}
