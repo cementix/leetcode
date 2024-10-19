ba2faeba5a2346e526df9f3c73f6ed25a269e9b4
Bohdan Kondratskyi <144433478+cementix@users.noreply.github.com>
2024-10-19 09:04:42 +0200

    Time: 1 ms (100%), Space: 51.9 MB (85.48%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -1,14 +1,14 @@
 var removeDuplicates = function(nums) {
-    let left = nums[0]
-    let k = 1
+    if (nums.length === 0) return 0; // Edge case: if array is empty, return 0
     
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] !== left) {
-            nums[k] = nums[i]
-            k++
-            left = nums[i]
+    let i = 0; // Slow pointer to track the position of the unique elements
+
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[j] !== nums[i]) { // When we find a new unique element
+            i++; // Move the slow pointer
+            nums[i] = nums[j]; // Place the unique element at position i
         }
     }
-    
-    return k
+
+    return i + 1; // i is the index, so length will be i + 1
 };
