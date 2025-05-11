d16c0a2031dc8c7168246774c99a603216477769
Bohdan Kondratskyi <144433478+cementix@users.noreply.github.com>
2025-05-11 14:40:31 +0200

    Time: 3 ms (62.91%), Space: 60.6 MB (55.52%) - LeetHub

---
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function(nums, k) {
+    let left = 0
+    let zeroCount = 0
+    let maxLength = 0
+
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] === 0) {
+            zeroCount++
+        }
+
+        while (zeroCount > k) {
+            if (nums[left] === 0) {
+                zeroCount--
+            }
+            left++
+        }
+
+        maxLength = Math.max(maxLength, right - left + 1)
+    }
+
+    return maxLength
+};
