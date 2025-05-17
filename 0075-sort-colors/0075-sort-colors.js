aa2c24adb8095a3fecda6961581d8bc4eed7fa59
Bohdan Kondratskyi <144433478+cementix@users.noreply.github.com>
2025-05-17 11:28:37 +0200

    Time: 0 ms (100%), Space: 53.8 MB (47.39%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,5 +3,24 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    nums.sort((a, b) => a - b)
+    let zero = 0, one = 0, two = 0;
+
+    for (const num of nums) {
+        if (num === 0) zero++;
+        else if (num === 1) one++;
+        else two++;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        if (zero > 0) {
+            nums[i] = 0
+            zero--
+        } else if (one > 0) {
+            nums[i] = 1
+            one--
+        } else {
+            nums[i] = 2
+            two--
+        }
+    }
 };
