a2073b83391e63b39d02ab3a93a884197d7ec85c
Bohdan Kondratskyi <144433478+cementix@users.noreply.github.com>
2025-11-02 13:00:41 +0100

    Time: 1 ms (72.51%), Space: 54.6 MB (42.13%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -1,17 +1,10 @@
-/**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
- */
 var isSubsequence = function(s, t) {
-    let length = 0
+    let i = 0, j = 0
 
-    for (let i = 0, j = 0; i < t.length; i++) {
-        if (t[i] === s[j]) {
-            j++
-            length++
-        }
+    while(j < t.length &&  i < s.length) {
+        if (s[i] === t[j]) i++
+        j++
     }
-
-    return length === s.length ? true : false
+    
+    return i === s.length
 };
