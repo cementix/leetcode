d86dc321b0b814239ef75a9ba72991dc03275785
Bohdan Kondratskyi <144433478+cementix@users.noreply.github.com>
2025-11-02 12:16:29 +0100

    Time: 14 ms (28.5%), Space: 74.1 MB (13.98%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -1,22 +1,39 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
+// var productExceptSelf = function(nums) {
+//     const n = nums.length
+//     const res = Array(n).fill(1)
+
+//     let left = 1
+//     for (let i = 0; i < n; i++) {
+//         res[i] = left
+//         left *= nums[i]
+//     }
+
+//     let right = 1
+//     for (let i = n - 1; i >= 0; i--) {
+//         res[i] *= right
+//         right *= nums[i]
+//     }
+
+//     return res
+// };
+
 var productExceptSelf = function(nums) {
     const n = nums.length
-    const result = Array(n).fill(1)
+    const left = Array(n).fill(1)
+    const right = Array(n).fill(1)
+    const res = Array(n).fill(1)
 
-    let prefix = 1
+    for (let i = 1; i < n; i++) {
+        left[i] = left[i - 1] * nums[i - 1]
+    }
+
+    for (let i = n - 2; i >= 0; i--) {
+        right[i] = right[i + 1] * nums[i + 1]
+    }
+
     for (let i = 0; i < n; i++) {
-        result[i] = prefix
-        prefix *= nums[i]
+        res[i] = left[i] * right[i]
     }
 
-    let postfix = 1
-    for (let i = n - 1; i >= 0; i--) {
-            result[i] *= postfix;
-        postfix *= nums[i];
-    }
-
-    return result
-};
+    return res
+}
