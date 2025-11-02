a0eb74624ae3abeb38baeb6b57337a19f9c74227
Bohdan Kondratskyi <144433478+cementix@users.noreply.github.com>
2025-11-02 12:49:53 +0100

    Time: 1 ms (76.46%), Space: 60.2 MB (39.9%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -1,14 +1,17 @@
-/**
- * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
- */
 var moveZeroes = function(nums) {
-    for (let l = 0, r = 0; r < nums.length; r++) {
-        if (nums[r] !== 0) {
-            const temp = nums[l]
-            nums[l] = nums[r]
-            nums[r] = temp
-            l++
+    let write = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[write] = nums[i]
+            write++
         }
     }
+
+    while (write < nums.length) {
+        nums[write] = 0
+        write++
+    }
+
+    return nums
 };
