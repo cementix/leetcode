24ed530415f204b397cfc653923b30da469c77bb
Bohdan Kondratskyi <144433478+cementix@users.noreply.github.com>
2025-11-07 11:41:39 +0100

    Time: 5 ms (29.17%), Space: 58.5 MB (21.38%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -4,18 +4,15 @@
  */
 var pivotIndex = function(nums) {
     let total = 0
-
-    for (const num of nums) {
-        total += num
+    for (const x of nums) {
+        total += x
     }
-    
+
     let leftSum = 0
 
     for (let i = 0; i < nums.length; i++) {
-        if (leftSum === total - leftSum - nums[i]) {
-            return i
-        }
-
+        const rightSum = total - leftSum - nums[i]
+        if (leftSum === rightSum) return i
         leftSum += nums[i]
     }
 
