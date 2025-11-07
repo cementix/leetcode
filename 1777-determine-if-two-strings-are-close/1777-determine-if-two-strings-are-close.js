9c1d75a290ff2e14f0e0f3e1361b0a3651b03443
Bohdan Kondratskyi <144433478+cementix@users.noreply.github.com>
2025-11-07 12:00:43 +0100

    Time: 66 ms (71.43%), Space: 61.7 MB (88.61%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.js
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.js
@@ -1,38 +1,28 @@
-/**
- * @param {string} word1
- * @param {string} word2
- * @return {boolean}
- */
-var closeStrings = function(word1, word2) {
-    if (word1.length !== word2.length) return false
-
-    const freq1 = {}
-    const freq2 = {}
+var closeStrings = function (word1, word2) {
+    const hash1 = {}
+    const hash2 = {}
 
     for (let i = 0; i < word1.length; i++) {
-        freq1[word1[i]] = (freq1[word1[i]] || 0) + 1
-        freq2[word2[i]] = (freq2[word2[i]] || 0) + 1
+        hash1[word1[i]] = (hash1[word1[i]] || 0) + 1
     }
 
-    for (const ch in freq1) {
-        if (!(ch in freq2)) {
-            return false
-        }
+    for (let i = 0; i < word2.length; i++) {
+        hash2[word2[i]] = (hash2[word2[i]] || 0) + 1
     }
 
-    for (const ch in freq2) {
-        if (!(ch in freq1)) {
-            return false
-        }
+    const keys1 = Object.keys(hash1).sort()
+    const keys2 = Object.keys(hash2).sort()
+
+    if (keys1.length !== keys2.length) return false
+    for (let i = 0; i < keys1.length; i++) {
+        if (keys1[i] !== keys2[i]) return false
     }
 
-    const counts1 = Object.values(freq1).sort((a, b) => a - b)
-    const counts2 = Object.values(freq2).sort((a, b) => a - b)
+    const arr1 = Object.values(hash1).sort((a, b) => a - b)
+    const arr2 = Object.values(hash2).sort((a, b) => a - b)
 
-    for (let i = 0; i < counts1.length; i++) {
-        if (counts1[i] !== counts2[i]) {
-            return false
-        }
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) return false
     }
 
     return true
