9fa7e727255a8076bbd8caaf91b2c82481402694
Bohdan Kondratskyi <144433478+cementix@users.noreply.github.com>
2025-11-07 12:03:57 +0100

    Time: 64 ms (76.13%), Space: 60.8 MB (97.83%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.js
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.js
@@ -10,13 +10,8 @@ var closeStrings = function (word1, word2) {
         hash2[word2[i]] = (hash2[word2[i]] || 0) + 1
     }
 
-    const keys1 = Object.keys(hash1).sort()
-    const keys2 = Object.keys(hash2).sort()
-
-    if (keys1.length !== keys2.length) return false
-    for (let i = 0; i < keys1.length; i++) {
-        if (keys1[i] !== keys2[i]) return false
-    }
+    if (Object.keys(hash1).sort().join('') !== Object.keys(hash2).sort().join(''))
+        return false
 
     const arr1 = Object.values(hash1).sort((a, b) => a - b)
     const arr2 = Object.values(hash2).sort((a, b) => a - b)
