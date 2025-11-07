355122fce7c27deff625cfa688cfdc03739d88a8
Bohdan Kondratskyi <144433478+cementix@users.noreply.github.com>
2025-11-07 11:47:33 +0100

    Time: 10 ms (51.97%), Space: 62.9 MB (17.72%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.js
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.js
@@ -1,20 +1,8 @@
-/**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number[][]}
- */
 var findDifference = function (nums1, nums2) {
-    const set1 = new Set()
-    const set2 = new Set()
+    const set1 = new Set(nums1)
+    const set2 = new Set(nums2)
+
     const res = [[], []]
-
-    for (const x of nums1) {
-        if (!set1.has(x)) set1.add(x)
-    }
-
-    for (const x of nums2) {
-        if (!set2.has(x)) set2.add(x)
-    }
 
     for (const val of set1) {
         if (!set2.has(val)) res[0].push(val)
