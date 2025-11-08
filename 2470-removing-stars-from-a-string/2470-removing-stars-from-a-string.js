a174244e8491090bc07e0176ab278d8b067a3ece
Bohdan Kondratskyi <144433478+cementix@users.noreply.github.com>
2025-11-08 08:23:00 +0100

    Time: 20 ms (80.14%), Space: 69.6 MB (38.32%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.js
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.js
@@ -1,15 +1,11 @@
-/**
- * @param {string} s
- * @return {string}
- */
 var removeStars = function(s) {
     const stack = []
 
-    for (let ch of s) {
-        if (ch === '*') {
+    for (const char of s) {
+        if (char === '*') {
             stack.pop()
         } else {
-            stack.push(ch)
+            stack.push(char)
         }
     }
 
