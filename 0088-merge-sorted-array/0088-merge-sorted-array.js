863947a2466b6f6dfb4673b97ae586f798f388ab
Bohdan Kondratskyi <144433478+cementix@users.noreply.github.com>
2025-11-19 20:12:45 +0100

    Time: 0 ms (100%), Space: 55.4 MB (25.82%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -1,25 +1,14 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
 var merge = function(nums1, m, nums2, n) {
-    // Start from the end of both arrays
-    let i = m - 1; // Last element in nums1 (excluding the extra zeros)
-    let j = n - 1; // Last element in nums2
-    let k = m + n - 1; // Last position in nums1
-
-    // Compare elements from the back of both arrays and place them in nums1
-    while (i >= 0 && j >= 0) {
-        if (nums1[i] > nums2[j]) {
-            nums1[k] = nums1[i];
-            i--;
-        } else {
-            nums1[k] = nums2[j];
-            j--;
-        }
-        k--;
+    for (let i = m, j = 0; i < m + n; i++, j++) {
+        nums1[i] = nums2[j]
     }
 
-    // If nums2 still has elements left, place them in nums1
-    while (j >= 0) {
-        nums1[k] = nums2[j];
-        j--;
-        k--;
-    }
+    return nums1.sort((a, b) => a - b)
 };
