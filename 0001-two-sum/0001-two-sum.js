43f101a7e2b576be4fadac4d1a968e0536d768b9
Bohdan Kondratskyi <144433478+cementix@users.noreply.github.com>
2026-02-18 09:47:15 +0100

    Time: 33 ms (31.12%), Space: 54.7 MB (61.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,12 +4,9 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  let l = nums.length
-  for (let i = 0; i < l; i++) {
-    for (let j = i + 1; j < l; j++) {
-        if (nums[i] + nums[j] == target) {
-            return [i, j]
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) return [i, j]
         }
     }
-  }  
 };
